9263b69c2dac4712626964b5886e21c8a7737d9c
iForneu <ikorotaevkirill@gmail.com>
2021-12-24 22:33:31 +0300

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Kirill Korotaev on 24.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
