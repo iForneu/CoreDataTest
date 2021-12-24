e95817d5bc5be665fc971e57d66ae2ab3496aa39
iForneu <ikorotaevkirill@gmail.com>
2021-12-24 23:25:28 +0300

    Create TaskViewController

---
+++ CoreDataTest/TaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TaskViewController.swift
+//  CoreDataTest
+//
+//  Created by Kirill Korotaev on 24.12.2021.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
